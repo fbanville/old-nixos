d552e6da108b04b7f319d1c19f007627b7bf7807
François Banville <waf@banber.org>
2023-10-08 12:30:14 -0400

    upgraded to unstable

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "François Banville NixOS config.";
   inputs = {
-    nixpkgs.url        = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url        = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    home-manager.url   = "github:nix-community/home-manager/release-23.05";
+    home-manager.url   = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, nix, nixos-hardware, home-manager }: {
