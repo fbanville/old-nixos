e5c1238ea918bfc2fed079e780b6e70423c10e65
François Banville <waf@banber.org>
2023-12-12 08:37:58 -0500

    satori deep sleep

--- hosts/satori.nix
+++ hosts/satori.nix
@@ -14,7 +14,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.editor = false;
   boot.loader.timeout = 0;
-  boot.kernelParams = ["quiet" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"];
+  boot.kernelParams = [
+    "quiet" 
+    "rd.systemd.show_status=false" 
+    "rd.udev.log_level=3" 
+    "udev.log_priority=3"
+    "mem_sleep_default=deep"
+    # "nvme.noacpi=1"
+  ];
+
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
   boot.initrd.systemd.enable = true;
