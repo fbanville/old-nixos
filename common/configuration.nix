9b43e49f1ff9ce6a956d2def957b5a20d7beae70
François Banville <waf@banber.org>
2024-02-10 11:23:20 -0500

    xserver.xkb configuration syntax change

--- common/configuration.nix
+++ common/configuration.nix
@@ -24,7 +24,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
     xkbVariant = "intl";
   };
